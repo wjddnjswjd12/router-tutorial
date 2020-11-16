23fc59dbc26de127b4760e3d846ed8828004865e
이정원 <wjddnjswjd12@gmail.com>
2020-11-16 13:47:16 +0900

    sdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
+import Profile from "./Profile";
 import Home from "./Home";
 
 const App = () => {
@@ -11,12 +12,19 @@ const App = () => {
           <Link to="/">홈</Link>
         </li>
         <li>
-          <Link to="/About">소개</Link>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">Velopert의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">Gildong의 프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
